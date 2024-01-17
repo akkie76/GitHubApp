3b01cb2ce81ad15f6562681fc29ba6e51a1bae05
akihiyo76 <ak.sato.info@gmail.com>
2024-01-17 23:50:51 +0900

    Initial commit

---
+++ GitHubApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//
+//  Copyright © 2024 akkie76. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubApp/GitHubApp.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubApp.swift
+//
+//  Copyright © 2024 akkie76. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
